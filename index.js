116d88e44239da8a1f1f874d042bb41fd7ba88d9
libin <libin@yxyt.com>
2020-06-05 16:50:06 +0800

    1.fixbug:shell文件找不到

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-var shell = require('shelljs');
+const shell = require('shelljs');
+const path = require('path')
 
 module.exports = function() {
-    shell.exec(Object.keys(arguments).reduce((a, b) => a + ` "${arguments[b]}"`, './helper.sh'));
+    shell.exec(Object.keys(arguments).reduce((a, b) => a + ` "${arguments[b]}"`, path.join(__dirname, './helper.sh')));
 }
